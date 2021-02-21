0908e9723682408f0e5c070c40ee566dae23ad99
majidux <majiddarvishnejad93@gmail.com>
2021-02-21 16:22:09 +0330

    Initial

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
+const log = console.log;
+
 function partsSums(ls: number[]) {
   const res: number[] = [];
   const list = ls;
   (function calculator(list) {
-    if (ls.length !== 0) {
+    if (ls.length !== 0 && list.length !== 0) {
       const red = list.reduce((item, acc) => item + acc);
       res.push(red);
       list.shift();
       calculator(list);
       return;
     }
+    res.push(0);
   })(list);
   return res;
 }
+
+log('My logs :');
+log(
+  partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358])
+);
