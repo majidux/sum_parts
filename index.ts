66563598154360748ffea1f97de87debd53c7237
majidux <majiddarvishnejad93@gmail.com>
2021-02-21 16:26:57 +0330

    feat : add better practice

--- index.ts
+++ index.ts
@@ -1,22 +1,12 @@
-const log = console.log;
+let ls = [0, 1, 3, 6, 10];
 
-function partsSums(ls: number[]) {
-  const res: number[] = [];
-  const list = ls;
-  (function calculator(list) {
-    if (ls.length !== 0 && list.length !== 0) {
-      const red = list.reduce((item, acc) => item + acc);
-      res.push(red);
-      list.shift();
-      calculator(list);
-      return;
-    }
-    res.push(0);
-  })(list);
+function partsSums(ls) {
+  let sum = ls.reduce((sum, n) => sum + n, 0);
+  let res = [sum];
+  for (let i = 1; i <= ls.length; i++) {
+    sum -= ls[i - 1];
+    res.push(sum);
+  }
   return res;
 }
-
-log('My logs :');
-log(
-  partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358])
-);
+console.log(partsSums(ls));
